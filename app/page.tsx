c56787f112a6baa52cf4fc0132fcca0cc864cf2e
kachman-git <onyekachiokpala67@gmail.com>
2024-11-26 10:27:09 +0000

    First Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlight from "./components/Highlight";
-import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItworks from "./components/HowItworks";
 import Footer from "./components/Footer";
@@ -12,7 +11,6 @@ const Home = () => {
       <Navbar />
       <Hero />
       <Highlight />
-      {/* <Model /> */}
       <Features />
       <HowItworks />
       <Footer />
