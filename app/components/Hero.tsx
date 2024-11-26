5173b058af952a917a3f5dc85634ed4f0036355c
kachman-git <onyekachiokpala67@gmail.com>
2024-11-26 11:39:34 +0000

    First Commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -7,8 +7,7 @@ import React, { useEffect, useState } from "react";
 import { heroVideo, smallHeroVideo } from "../utils";
 
 const Hero = () => {
-  const vid = window.innerWidth < 760 ? smallHeroVideo : heroVideo;
-  const [videoSrc, setVideoSrc] = useState(vid);
+  const [videoSrc, setVideoSrc] = useState("");
 
   const handleVideoSrcSet = () => {
     if (window.innerWidth < 760) {
